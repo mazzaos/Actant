b2c1043f5b21c9b839b9aaf54b60eb8ec2169b7b
Lorenzo Mazzarotto <mazzaos@icloud.com>
2022-06-02 14:52:40 +0200

    Initial Commit

---
+++ Actant/ActantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActantApp.swift
+//  Actant
+//
+//  Created by Lorenzo Lins Mazzarotto on 02/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ActantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Actant/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Actant
+//
+//  Created by Lorenzo Lins Mazzarotto on 02/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
